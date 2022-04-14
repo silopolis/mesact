a1090f4be164d6af7a71f42b39db4fa6958c65b5
John Thornton <jthornton@gnipsel.com>
2022-04-14 12:15:16 -0500

    fix typo
    Signed-off-by: John Thornton <jthornton@gnipsel.com>

--- mesact/src/libmesact/utilities.py
+++ mesact/src/libmesact/utilities.py
@@ -710,17 +710,17 @@ def ferrorSetDefault(parent):
 	if not parent.linearUnitsCB.currentData():
 		QMessageBox.warning(parent,'Warning', 'Machine Tab\nLinear Units\nmust be selected', QMessageBox.Ok)
 		return
-	connector = parent.sender().objectName()[:3]
+	connector = parent.sender().objectName()[:2]
 	joint = parent.sender().objectName()[-1]
 	if parent.linearUnitsCB.currentData() == 'inch':
-		getattr(parent, f'{connector}ferror_{joint}').setText(' 0.0002')
-		getattr(parent, f'{connector}min_ferror_{joint}').setText(' 0.0001')
+		getattr(parent, f'{connector}_ferror_{joint}').setText(' 0.0002')
+		getattr(parent, f'{connector}_min_ferror_{joint}').setText(' 0.0001')
 	else:
-		getattr(parent, f'{connector}ferror_{joint}').setText(' 0.005')
-		getattr(parent, f'{connector}min_ferror_{joint}').setText(' 0.0025')
+		getattr(parent, f'{connector}_ferror_{joint}').setText(' 0.005')
+		getattr(parent, f'{connector}_min_ferror_{joint}').setText(' 0.0025')
 
 def pidSetDefault(parent):
-	connector = parent.sender().objectName()[:3]
+	connector = parent.sender().objectName()[:2]
 	joint = parent.sender().objectName()[-1]
 	if not parent.linearUnitsCB.currentData():
 		QMessageBox.warning(parent,'Warning', 'Machine Tab\nLinear Units\nmust be selected', QMessageBox.Ok)
@@ -739,26 +739,28 @@ def pidSetDefault(parent):
 		return
 
 	p = int(1000/(int(parent.servoPeriodSB.cleanText())/1000000))
-	getattr(parent,  f'{connector}p_{joint}').setText(f'{p}')
-	getattr(parent, f'{connector}i_{joint}').setText('0')
-	getattr(parent, f'{connector}d_{joint}').setText('0')
-	getattr(parent, f'{connector}ff0_{joint}').setText('0')
-	getattr(parent, f'{connector}ff1_{joint}').setText('1')
-	getattr(parent, f'{connector}ff2_{joint}').setText('0')
-	getattr(parent, f'{connector}bias_{joint}').setText('0')
-	getattr(parent, f'{connector}maxOutput_{joint}').setText('0')
+	getattr(parent,  f'{connector}_p_{joint}').setText(f'{p}')
+	getattr(parent, f'{connector}_i_{joint}').setText('0')
+	getattr(parent, f'{connector}_d_{joint}').setText('0')
+	getattr(parent, f'{connector}_ff0_{joint}').setText('0')
+	getattr(parent, f'{connector}_ff1_{joint}').setText('1')
+	getattr(parent, f'{connector}_ff2_{joint}').setText('0')
+	getattr(parent, f'{connector}_bias_{joint}').setText('0')
+	getattr(parent, f'{connector}_maxOutput_{joint}').setText('0')
 	if parent.linearUnitsCB.itemData(parent.linearUnitsCB.currentIndex()) == 'inch':
 		maxError = '0.0005'
 	else:
 		maxError = '0.0127'
-	getattr(parent, f'{connector}maxError_{joint}').setText(maxError)
-	getattr(parent, f'{connector}deadband_{joint}').setText('0')
+	getattr(parent, f'{connector}_maxError_{joint}').setText(maxError)
+	getattr(parent, f'{connector}_deadband_{joint}').setText('0')
 
-def analogSetDefault(parent):
-	tab = parent.sender().objectName()[-1]
-	getattr(parent, 'analogMinLimit_' + tab).setText('-10')
-	getattr(parent, 'analogMaxLimit_' + tab).setText('10')
-	getattr(parent, 'analogScaleMax_' + tab).setText('10')
+def analogSetDefault(parent): # think this is broken...
+	#tab = parent.sender().objectName()[-1]
+	connector = parent.sender().objectName()[:2]
+	joint = parent.sender().objectName()[-1]
+	getattr(parent, f'{connector}_analogMinLimit_{joint}').setText('-10')
+	getattr(parent, f'{connector}_analogMaxLimit_{joint}').setText('10')
+	getattr(parent, f'{connector}_analogScaleMax_{joint}').setText('10')
 
 def driveChanged(parent):
 	timing = parent.sender().itemData(parent.sender().currentIndex())
